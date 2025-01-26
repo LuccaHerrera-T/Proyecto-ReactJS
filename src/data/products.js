1b3a891249914453f2885f69e4e3cb6a6f590233
Lucca Herrera <luccaherrera.tempest@gmail.com>
2025-01-25 22:59:06 -0300

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,28 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Laptop',
+      category: 'electronica',
+      description: 'Laptop de alta gama ideal para trabajo y gaming.',
+      price: 1200,
+      image: '/images/laptop.jpg',
+    },
+    {
+      id: 2,
+      name: 'Camisa Casual',
+      category: 'ropa',
+      description: 'Camisa de algodón ideal para el día a día.',
+      price: 35,
+      image: '/images/camisa.jpg',
+    },
+    {
+      id: 3,
+      name: 'Cafetera',
+      category: 'hogar',
+      description: 'Cafetera automática para preparar café delicioso.',
+      price: 80,
+      image: '/images/cafetera.jpg',
+    },
+    // Agrega más productos según sea necesario
+  ];
+  
