2515b2116e30fd2b613b3ea8543e38913e3cc480
Lucca Herrera <luccaherrera.tempest@gmail.com>
2025-01-25 22:55:17 -0300

    Delete src directory

--- src/pages/Shop.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ItemListContainer from '../components/ItemListContainer';
-
-const Shop = ({ products, addToCart }) => {
-  return <ItemListContainer products={products} addToCart={addToCart} />;
-};
-
-export default Shop;
